a748b753bf25dff6c5930cace324f562ef7e778b
Johny Boy <johny@boy.com>
2021-10-05 13:19:16 -0400

    Clean up

--- faye-redis.js
+++ faye-redis.js
@@ -39,12 +39,12 @@ var Engine = function(server, options) {
   var self = this;
   this._subscriber.subscribe(this._messageChannel);
   this._subscriber.subscribe(this._closeChannel);
-  this._subscriber.on('message', function(topic, message) {
+  this._subscriber.on('message', (topic, message) => {
     if (topic === self._messageChannel) self.emptyQueue(message);
     if (topic === self._closeChannel)   self._server.trigger('close', message);
   });
 
-  this._gc = setInterval(function() { self.gc(); }, gc * 1000);
+  this._gc = setInterval(() => { self.gc(); }, gc * 1000);
 };
 
 Engine.create = function(server, options) {
@@ -63,7 +63,7 @@ Engine.prototype = {
       this._redis.disconnect(),
       this._subscriber.disconnect()
     ])
-    .nodeify(function(err) {
+    .nodeify((err) => {
       if (err) console.error(err.stack);
     });
 
@@ -71,9 +71,8 @@ Engine.prototype = {
 
   _newClientId: function() {
     var clientId = this._server.generateId();
-    return this._redis.zscore(this._clientsKey, clientId)
-      .bind(this)
-      .then(function(timeout) {
+    return Promise.resolve(this._redis.zscore(this._clientsKey, clientId))
+      .then((timeout) => {
         if (timeout !== null) return this._newClientId();
         return clientId;
       });
@@ -81,17 +80,15 @@ Engine.prototype = {
 
   createClient: function(callback, context) {
     return this._newClientId()
-      .bind(this)
-      .then(function(clientId) {
+      .then((clientId) => {
         return this._updateClientTTL(clientId)
-          .bind(this)
-          .then(function() {
+          .then(() => {
             this._server.debug('Created new client ?', clientId);
             this._server.trigger('handshake', clientId);
             return clientId;
           });
       })
-      .nodeify(function(err, clientId) {
+      .nodeify((err, clientId) => {
         if (err) console.error(err.stack);
         callback.call(context, clientId);
       });
@@ -106,9 +103,8 @@ Engine.prototype = {
   },
 
   destroyClient: function(clientId, callback, context) {
-    return this._redis.smembers(this._clientChannelsKey(clientId))
-      .bind(this)
-      .then(function(channels) {
+    return Promise.resolve(this._redis.smembers(this._clientChannelsKey(clientId)))
+      .then((channels) => {
         var multiClient = this._redis.multi();
 
         channels.forEach(function(channel) {
@@ -119,7 +115,7 @@ Engine.prototype = {
 
         return [channels, multiClient.exec()];
       })
-      .spread(function(channels, results) {
+      .spread((channels, results) => {
         channels.forEach(function(channel, i) {
           var result = results[i][1];
           if (result !== 1) return;
@@ -138,11 +134,11 @@ Engine.prototype = {
 
         return multi.exec();
       })
-      .then(function() {
+      .then(() => {
         this._server.debug('Destroyed client ?', clientId);
         this._server.trigger('disconnect', clientId);
       })
-      .nodeify(function(err) {
+      .nodeify((err) => {
         if (err) console.error(err.stack);
         if (callback) callback.call(context);
       });
@@ -167,12 +163,11 @@ Engine.prototype = {
         this._redis.sadd(this._clientChannelsKey(clientId), channel),
         this._redis.sadd(this._channelKey(channel), clientId)
       ])
-      .bind(this)
-      .spread(function(added) {
+      .spread((added) => {
         if (added === 1) this._server.trigger('subscribe', clientId, channel);
         this._server.debug('Subscribed client ? to channel ?', clientId, channel);
       })
-      .nodeify(function(err) {
+      .nodeify((err) => {
         if (err) console.error(err.stack);
         if (callback) callback.call(context);
       });
@@ -183,12 +178,11 @@ Engine.prototype = {
         this._redis.srem(this._clientChannelsKey(clientId), channel),
         this._redis.srem(this._channelKey(channel), clientId)
       ])
-      .bind(this)
-      .spread(function(removed) {
+      .spread((removed) => {
         if (removed === 1) this._server.trigger('unsubscribe', clientId, channel);
         this._server.debug('Unsubscribed client ? from channel ?', clientId, channel);
       })
-      .nodeify(function(err) {
+      .nodeify((err) => {
         if (err) console.error(err.stack);
         if (callback) callback.call(context);
       });
@@ -203,25 +197,23 @@ Engine.prototype = {
     this._server.trigger('publish', message.clientId, message.channel, message.data);
 
     return this._redis.sunion(keys)
-      .bind(this)
-      .then(function(clients) {
+      .then((clients) => {
         if (!clients || !clients.length) return;
 
-        return Promise.map(clients, function(clientId) {
+        return Promise.map(clients, (clientId) => {
             var queue = this._clientMessagesKey(clientId);
             this._server.debug('Queueing for client ?: ?', clientId, message);
 
             return this._redis.rpush(queue, jsonMessage);
             // TODO: figure out what to do with this
-            // this.clientExists(clientId, function(exists) {
+            // this.clientExists(clientId, (exists) => {
             //   if (!exists) this._redis.del(queue);
             // });
-          }.bind(this))
-          .bind(this)
-          .then(function() {
+          })
+          .then(() => {
             var pipeline = this._redis.pipeline();
             var messageChannel = this._messageChannel;
-            clients.forEach(function(clientId) {
+            clients.forEach((clientId) => {
               pipeline.publish(messageChannel, clientId);
             });
 
@@ -240,11 +232,10 @@ Engine.prototype = {
     multi.del(key);
 
     return multi.exec()
-      .bind(this)
-      .then(function(results) {
+      .then((results) => {
         var jsonMessages = results[0][1];
         if (!jsonMessages) return;
-        var messages = jsonMessages.map(function(json) { return JSON.parse(json); });
+        var messages = jsonMessages.map((json) => { return JSON.parse(json); });
         this._server.deliver(clientId, messages);
       });
   },
@@ -254,17 +245,16 @@ Engine.prototype = {
     if (typeof timeout !== 'number') return;
     if (this._disconnected) return;
 
-    return this._withLock('gc', function() {
+    return this._withLock('gc', () => {
       var cutoff = new Date().getTime() - 1000 * 2 * timeout;
 
       return this._redis.zrangebyscore(this._clientsKey, 0, cutoff)
-        .bind(this)
-        .then(function(clients) {
-          return Promise.map(clients, function(clientId) {
+        .then((clients) => {
+          return Promise.map(clients, (clientId) => {
             return this.destroyClient(clientId);
-          }.bind(this));
+          });
         });
-    }.bind(this));
+    });
   },
 
   _withLock: function(lockName, callback, context) {
@@ -273,8 +263,7 @@ Engine.prototype = {
         expiry      = currentTime + this.LOCK_TIMEOUT * 1000 + 1;
 
     return this._redis.setnx(lockKey, expiry)
-      .bind(this)
-      .then(function(set) {
+      .then((set) => {
         if (this._disconnected) return false;
 
         if (set === 1) {
@@ -282,8 +271,7 @@ Engine.prototype = {
         }
 
         return this._redis.get(lockKey)
-          .bind(this)
-          .then(function(timeout) {
+          .then((timeout) => {
             if (this._disconnected) return false;
             if (!timeout) return false;
 
@@ -291,8 +279,7 @@ Engine.prototype = {
             if (currentTime < lockTimeout) return false;
 
             return this._redis.getset(lockKey, expiry)
-              .bind(this)
-              .then(function(oldValue) {
+              .then((oldValue) => {
                 if (this._disconnected) return false;
 
                 if (oldValue !== timeout) return false;
@@ -300,12 +287,11 @@ Engine.prototype = {
               });
           });
       })
-      .then(function(lockObtained) {
+      .then((lockObtained) => {
         if (!lockObtained) return;
 
         return Promise.try(callback)
-          .bind(this)
-          .finally(function() {
+          .finally(() => {
             if (this._disconnected) return;
             if (new Date().getTime() < expiry) this._redis.del(lockKey);
           });
